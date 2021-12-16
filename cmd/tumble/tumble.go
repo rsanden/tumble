bebe277710054324ac14da9adbebf02d47aad15e
Ryan Sanden <ryan.sanden@thinkdeca.com>
2021-12-16 18:08:17 +0800

    fight with gofmt

--- cmd/tumble/tumble.go
+++ cmd/tumble/tumble.go
@@ -21,12 +21,12 @@ var (
 )
 
 func init_globals() {
-	flag.StringVar(&logfile, "logfile", "", "path to logfile (required)")
-	flag.UintVar(&maxLogSize, "max-log-size", 0, "max log size before rotation (in MB) (required)")
-	flag.UintVar(&maxTotalSize, "max-total-size", 0, "max total size before deletion (in MB) (required)")
-	flag.BoolVar(&isTeeStdout, "tee-stdout", false, "tee to stdout")
-	flag.BoolVar(&isTeeStderr, "tee-stderr", false, "tee to stderr")
-	flag.StringVar(&timeFormat, "time-format", "", "add timestamp with given format (default: no timestamp) (example: '2006-01-02 15:04:05.000')")
+	flag.StringVar(&logfile /*****/, "logfile" /*********/, "" /*****/, "path to logfile (required)")
+	flag.UintVar(&maxLogSize /****/, "max-log-size" /****/, 0 /******/, "max log size before rotation (in MB) (required)")
+	flag.UintVar(&maxTotalSize /**/, "max-total-size" /**/, 0 /******/, "max total size before deletion (in MB) (required)")
+	flag.BoolVar(&isTeeStdout /***/, "tee-stdout" /******/, false /**/, "tee to stdout")
+	flag.BoolVar(&isTeeStderr /***/, "tee-stderr" /******/, false /**/, "tee to stderr")
+	flag.StringVar(&timeFormat /**/, "time-format" /*****/, "" /*****/, "add timestamp with given format (default: no timestamp) (example: '2006-01-02 15:04:05.000')")
 	flag.Parse()
 
 	if logfile == "" || maxLogSize == 0 || maxTotalSize == 0 {
